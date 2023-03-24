c28be1fe119235e422e3f383e21d790e0d1a21c4
Ray Kazuya Murphy <85945564+Rayezor@users.noreply.github.com>
2023-03-24 16:35:41 +0000

    Add project files.

---
+++ M50Tolls/Controllers/TollController.cs
@@ -0,0 +1,26 @@
+﻿using M50Tolls.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace M50Tolls.Controllers
+{
+    public class TollController : Controller
+    { 
+        // GET: TollController/Create
+        public ActionResult Calculate()
+        {
+            ViewBag.VehicleType = new SelectList(Vehicle.VehicleTypes);
+            return View(new Vehicle {VehicleType = "Car", HasETag = false});
+        }
+
+        // POST: TollController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Calculate(Vehicle vehicle)
+        {
+            ViewBag.VehicleType = new SelectList(Vehicle.VehicleTypes);
+            return View(vehicle);
+        }
+    }
+}
---
+++ M50Tolls/Models/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿namespace M50Tolls.Models
+{
+    public class Vehicle
+    {
+        const double CarTollCharge = 2.00;
+        const double PSVTollCharge = 2.80;
+        const double BusTollCharge = 2.00;
+
+        const double GoodsTollCharge = 4.10;
+
+        const double DiscountPercentage = 0.2;
+
+        public static String[] VehicleTypes
+        {
+            get
+            {
+                return new String[] { "Car", "PSV", "Bus", "Goods" };
+            }
+        }
+
+        public String VehicleType { get; set; }
+        public bool HasETag { get; set; }
+        public double Charge
+        {
+            get
+            {
+                double toll = 0;
+                if (VehicleType == "Car")
+                {
+                    toll = CarTollCharge;
+                }
+                else if (VehicleType == "PSV")
+                {
+                    toll = PSVTollCharge;
+                }
+                else if(VehicleType == "Bus")
+                {
+                    toll = BusTollCharge;
+                }
+                else if(VehicleType == "Goods")
+                {
+                    toll = GoodsTollCharge;
+                }
+
+                if (HasETag)
+                {
+                    toll *= (1.0 - DiscountPercentage);
+                }
+                return toll;
+            }
+        }
+    }
+}
